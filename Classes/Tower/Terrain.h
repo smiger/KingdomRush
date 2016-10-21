52031a46990d36b1071b509724f842f2111d5b5b
smiger <530509325@qq.com>
2016-10-21 09:06:21 +0800

    smiger commit

---
+++ Classes/Tower/Terrain.h
@@ -0,0 +1,23 @@
+#ifndef _TERRAIN_H_
+#define _TERRAIN_H_
+
+#include "cocos2d.h"
+
+USING_NS_CC;
+namespace xh{
+	class Terrain : public Sprite{
+
+	public:
+		virtual bool init(int type);
+		static xh::Terrain* createTerrain(int type);
+		void showUpdateMenu();
+		void hideUpdateMenu();
+		Sprite* terrain;
+		bool onTouchBegan(Touch *touch, Event *event);
+		void onTouchEnded(Touch* touch, Event* event);
+		bool isUpdateMenuShown;
+		void smokeEffect();
+	};
+}
+
+#endif
